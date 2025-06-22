2ca51f8907f3e6bd8e6c19d6f396cce5d5c545b3
ramanuja <mvangeepuram@gmail.com>
2025-06-22 13:38:22 -0700

    added new examples for python data types

--- 01_Day_Introduction/helloworld.py
+++ 01_Day_Introduction/helloworld.py
@@ -21,3 +21,15 @@ print(type({9.8, 3.14, 2.7}))    # Set
 print(type((9.8, 3.14, 2.7)))    # Tuple
 print(type(3 == 3))              # Bool
 print(type(3 >= 3))              # Bool
+
+print('***********medha\'s additions****************')
+print(type(15))                  # Int
+print(type(2.87))                # Float
+print(type(9 + 7j))              # Complex
+print(type('soreness'))          # String
+print(type(['monday', 'wednesday','thursday']))           # List
+print(type({'name' : 'medha', 'dad':'chary'})) # Dictionary
+print(type({'she', 'him', 'theirs'}))    # Set
+print(type(('a', 'b', 'c')))    # Tuple
+print(type(7 == 8))              # Bool
+print(type(10 >= 6))              # Bool
